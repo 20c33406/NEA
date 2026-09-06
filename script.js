8e72f4f40d728eb8fb42be27896a36f29c3f988d
Seth Sharma <sethsharma@Mac.lan>
2026-09-06 16:26:04 +0100

    Added cycles 4 and 5

--- script.js
+++ script.js
@@ -29,13 +29,13 @@ class Vector {
     this.X = 0;
     this.Y = 0;
   }
-  
+
 }
 
 
 
 class Object {
-  // Constructor for all of the attributes 
+  // Constructor for all of the attributes
   // that will be required to render the objects
   constructor(color, mass, radius, pos, vel, acc) {
     this.color = color;
@@ -46,7 +46,47 @@ class Object {
     this.acc = acc;
   }
 
-  // This method updates the positions 
+  calculateCollision(object) {
+    let dist = Math.hypot(this.pos.X - object.pos.X, this.pos.Y - object.pos.Y)
+    if (dist < this.radius + object.radius) {
+
+      let angle = -Math.atan2(object.pos.Y - this.pos.Y, object.pos.X - this.pos.X)
+
+      // First, rotate the system by the angle
+      // to align it to the x axis using vector multiplication
+
+      let thisRotatedVelocity = this.vel.rotate(angle)
+      let objRotatedVelocity = object.vel.rotate(angle)
+      let e = restitution;
+      let u1 = thisRotatedVelocity.Y;
+      let u2 = objRotatedVelocity.;
+
+      let m1 = this.mass;
+      let m2 = object.mass;
+
+      let v1 = (e * m2 * (u2 - u1) + m1 * u1 + m2 * u2) / (m1 + m2);
+      let v2 = (e * m1 * (u1 - u2) + m1 * u1 + m2 * u2) / (m1 + m2);
+
+      let dv1 = v1 - u1
+      let dv2 = v2 - u2
+
+      let thisRotatedAcceleration = new Vector(dv1, 0)
+      let objRotatedAcceleration = new Vector(dv2, 0)
+
+      let thisAcceleration = thisRotatedAcceleration.rotate(-angle)
+      let objAcceleration = objRotatedAcceleration.rotate(-angle)
+
+      this.acc.X += thisAcceleration.X
+      object.acc.X += objAcceleration.X
+      this.acc.Y += thisAcceleration.Y
+      object.acc.Y += objAcceleration.Y
+
+
+    }
+
+  }
+
+  // This method updates the positions
   // and velocities of objects using leapfrog integration
   leapfrogIntegration() {
     this.vel.X += this.acc.X/2;
@@ -95,7 +135,7 @@ class Player extends Object {
     this.thrust = 0;
 
     // Maximum acceleration of the craft
-    this.acceleration = 1;
+    this.acceleration = 0.01;
   }
 
   renderObject () {
@@ -178,12 +218,22 @@ class Player extends Object {
     if(this.angle>Math.PI*2){
       this.angle -= Math.PI*2
     }
+
+    if (this.thrust > 1) {
+      this.thrust = 1
+    } else if (this.thrust < 0) {
+      this.thrust = 0
+    }
+
+    
+    this.acc.X += this.acceleration * this.thrust * Math.cos(this.angle)
+    this.acc.Y -= this.acceleration * this.thrust * Math.sin(this.angle)
   }
 
 }
 
 function updateObjects() {
-  
+  player.applyRocketControls();
   for(let i = 0; i < objects.length; i++) {
     objects[i].leapfrogIntegration();
   }
@@ -199,13 +249,17 @@ function draw(time) {
   // Clears the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Checks all of the registered inputs
+
+
   // Updates the positions and velocities of the objects
   updateObjects();
-  
+
   // Renders the objects on the canvas
   renderObjects();
 
-  // Requests the next frame to be drawn, 
+
+  // Requests the next frame to be drawn,
   // creating a loop repeating 60 times per second
   requestAnimationFrame(draw);
 }
@@ -254,10 +308,26 @@ function init() {
   draw();
 }
 
-objects.push(new Player("red", 100, 100, new Vector(0,0), new Vector(0,0), new Vector(0,0), 1))
+let player = new Player("red", 0, 0, new Vector(0, -300), new Vector(0, 0), new Vector(0, 0), 1)
+
+objects.push(new Object("blue", 1000, 100, new Vector(-250,0), new Vector(0,0), new Vector(0,0), 1))
+objects.push(new Object("red", 1000, 100, new Vector(250,0), new Vector(0,0), new Vector(0,0), 1))
+
+/*
+objects.push(new Object("blue", 100000, 100, new Vector(-250,0), new Vector(0,0), new Vector(0,0), 1))
+objects.push(new Object("red", 1000, 100, new Vector(250,0), new Vector(0,0), new Vector(0,0), 1))
+*/
+
+/*
+objects.push(new Object("blue", 1000, 100, new Vector(-250,0), new Vector(0,0), new Vector(0,-1), 1))
+objects.push(new Object("red", 1000, 100, new Vector(250,0), new Vector(0,0), new Vector(0,-1), 1))
+*/
+
+/*
+objects.push(new Object("blue", 1000, 100, new Vector(-250,0), new Vector(0,0), new Vector(1,0), 1))
+objects.push(new Object("red", 1000, 100, new Vector(250,0), new Vector(0,0), new Vector(0,-3), 1))
+objects.push(new Object("red", 1000, 100, new Vector(0,500), new Vector(0,0), new Vector(2,0), 1))
+*/
+
+objects.push(player)
 draw();
-
-
-
-
-
