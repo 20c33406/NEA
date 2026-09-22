3655d15465de2dbbf0a3604cffd05e92d0a773ba
Seth Sharma <sethsharma@Seths-MacBook-Neo.local>
2026-09-22 13:07:38 +0100

    Completed cycle 9

---
+++ scenarioCreator/script.js
@@ -0,0 +1,87 @@
+let configArray = []
+
+const configInput = document.getElementById('configInput')
+
+const usePlayer = document.getElementById('player')
+const doGravity = document.getElementById('gravity')
+const doCollisions = document.getElementById('collisions')
+
+function copyConfig() {
+
+  const configs = document.getElementById('configs')
+  const cards = configs.getElementsByClassName('card')
+
+  let config = [[player.value, gravity.value, collisions.value]]
+  for (let i = 0; i < cards.length; i++) {
+    let cardAttributes = []
+    let attributes = cards[i].getElementsByClassName('input')
+    for (let j = 0; j < attributes.length; j++){
+
+      cardAttributes.push(attributes[j].value)
+
+    }
+    config.push(cardAttributes)
+  }
+
+  let JSONconfig = JSON.stringify(config)
+
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(JSONconfig);
+
+  // Alert the copied text
+  alert("Copied the text: " + JSONconfig);
+}
+
+/*
+  var configs = document.getElementById('configs');
+ var cards = configs.getElementsByClassName('card');
+ //
+ for (let j = 0; j < cards.length; j++){
+   let obj = []
+   let inputs = cards[j].getElementsByTagName('input')
+   for (let i = 0; i < inputs.length;i++){
+     obj.push(inputs[i].value)
+   }
+   configArray.push(obj)
+ }
+
+ // Copy the text inside the text field
+
+ navigator.clipboard.writeText(JSON.stringify(configArray));
+ alert("Copied the text: " + JSON.stringify(configArray));
+
+
+ configArray = []
+ */
+
+function createCard() {
+  const configs = document.getElementById('configs');
+  const cards = configs.getElementsByClassName('card');
+  const clonedCard = cards[0].cloneNode(true);
+  for (let i = 0; i < cards[0].getElementsByTagName('input').length;i++){
+    clonedCard[0].getElementsByTagName('input')[i].value = ''
+  }
+  configs.appendChild(clonedCard);
+};
+
+function loadConfig() {
+  const JSONconfig = configInput.value
+  const config = JSON.parse(JSONconfig)
+
+  const configs = document.getElementById('configs')
+  const cards = configs.getElementsByClassName('card')
+
+  player.checked = config[0][0]
+  gravity.checked = config[0][1]
+  collsions.checked = config[0][2]
+
+  for (let i = 1; i < config.length; i++){
+    const clonedCard = cards[0].cloneNode(true);
+    for (let j = 0; j < cards[0].getElementsByTagName('input').length;j++){
+      clonedCard[0].getElementsByTagName('input')[j].value = config[i][j]
+    }
+    configs.appendChild(clonedCard);
+  }
+  cards[0].remove()
+}
--- script.js
+++ script.js
@@ -1,16 +1,45 @@
+// Defines the HTML objects needed for canvas
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-const energy = document.getElementById("energy")
+// Reference to the body tag for deceting zoom event
+const body = document.querySelector("body");
 
+// The gameSpeedValue tag which is updated when the game speed is changed
+const gameSpeedValue = document.getElementById("gameSpeedValue")
+
+const configInput = document.getElementById('configInput')
+
+// The location of the centre of the canvas
 const centreY = canvas.height / 2;
 const centreX = canvas.width / 2
 
-const GCONST = 100
+// The minimum radius an object can be on the screen before its symbol appears
+const minimumRadius = 1.5;
 
-const restitution = 0.3;
+// The constant that defines the speed of the scrolling
+const scaleFactor = 1/1000;
 
+// The gravitational constant of the simulation
+const GCONST = 6.67*(10**-11);
+
+// The coefficient of restitution between all of the objects in the game
+const restitution = 1;
+
+// The current game speed multiplier
+let gameSpeed = 1;
+
+// The current scale of the camera view
+let scale = 1;
+
+let doCollisions = true
+let doGravity = true
+let usePlayer = true
+
+//
 let objects = [];
+
+let player;
 
 
 // Dictionary of pressed keys
@@ -66,19 +95,19 @@ class Object {
     this.pos = pos;
     this.vel = vel;
     this.acc = acc;
+
   }
 
   calculateGravity(object) {
-    if (this.mass == 0 || object.mass == 0) { return }
+
 
     let dist = Math.hypot(this.pos.X - object.pos.X, this.pos.Y - object.pos.Y);
     let angle = Math.atan2(object.pos.Y - this.pos.Y, object.pos.X - this.pos.X);
 
     if (dist < this.radius + object.radius) { return }
 
-
-    let thisMagnitude = GCONST * object.mass / (dist ** 2);
-    let objMagnitude = GCONST * this.mass / (dist ** 2);
+    let thisMagnitude = (GCONST * object.mass) / (dist ** 2);
+    let objMagnitude = (GCONST * this.mass) / (dist ** 2);
 
     let thisAcceleration = new Vector(thisMagnitude, 0).rotate(angle)
     let objAcceleration = new Vector(objMagnitude, 0).rotate(angle + Math.PI)
@@ -137,6 +166,7 @@ class Object {
 
 
   renderObject() {
+
     // Saves the current canvas configuration
     ctx.save();
 
@@ -158,6 +188,34 @@ class Object {
     // Restores the original canvas state
     ctx.restore();
   }
+
+  renderSymbol() {
+    ctx.save();
+
+    ctx.beginPath();
+    // Translates the origin to the position
+    // of the object relative to the centre of the canvas
+    ctx.translate(centreX + ((this.pos.X - player.pos.X)*scale),
+      centreY - ((-this.pos.Y + player.pos.Y)*scale));
+
+    // Creates the circle
+    ctx.arc(0, 0, 10, 0, 2 * Math.PI);
+
+    // Makes the fill colour correct
+    ctx.fillStyle = this.color;
+
+    // Makes the circle transparent
+    ctx.globalAlpha = 0.5
+
+    // Fills the circle
+    ctx.fill();
+
+    // Makes the pen opaque again
+    ctx.globalAlpha = 1
+
+    // Restores the original canvas state
+    ctx.restore();
+  }
 }
 
 class Player extends Object {
@@ -170,7 +228,10 @@ class Player extends Object {
     this.thrust = 0;
 
     // Maximum acceleration of the craft
-    this.acceleration = 0.01;
+    this.acceleration = 100;
+
+    //
+    this.sas = 'na'
   }
 
   renderObject () {
@@ -228,7 +289,41 @@ class Player extends Object {
     ctx.restore();
   }
 
+  renderSymbol() {
+    ctx.save();
 
+    ctx.translate(centreX, centreY);
+    ctx.rotate(- this.angle + Math.PI/2);
+    ctx.translate(-centreX, -centreY);
+
+    ctx.fillStyle = "green";
+    ctx.globalAlpha = 0.5;
+
+    ctx.beginPath();
+    ctx.moveTo(centreX, centreY - 15);
+    ctx.lineTo(centreX - 10, centreY + 15);
+    ctx.lineTo(centreX + 10, centreY + 15);
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.globalAlpha = 1;
+
+    ctx.restore();
+  }
+
+  getSOI() {
+    let soiEffect = -1;
+    let soi = 0;
+    for (let i = 0; i < objects.length; i++){
+      let dist = Math.hypot(objects[i].pos.Y - this.pos.Y, objects[i].pos.X - this.pos.X)
+      let effect = dist**2 / objects[i].mass
+      if ((effect < soiEffect || soiEffect == -1) && dist!=0) {
+        soiEffect = effect
+        soi = i
+      }
+    }
+    return objects[soi]
+  }
 
   applyRocketControls(){
     // Checks if keys are pressed down. If true apply the correct effects
@@ -244,17 +339,52 @@ class Player extends Object {
     if(keys.d){
       this.angularVelocity -= 0.0001
     }
-    // Loops the angle when it reaches 2pi radians
-    this.angle += this.angularVelocity
-    if(this.angle>Math.PI*2){
-      this.angle -= Math.PI*2
+
+    let soi = this.getSOI()
+    let newAngle = this.angle
+    switch (this.sas) {
+      case 'pg':
+        newAngle = -Math.atan2(this.vel.Y - soi.vel.Y, this.vel.X - soi.vel.X)
+        break
+      case 'rg':
+        newAngle = -Math.atan2(this.vel.Y - soi.vel.Y, this.vel.X - soi.vel.X) + Math.PI
+        break
+      case 'no':
+        newAngle = -Math.atan2(this.vel.Y - soi.vel.Y, this.vel.X - soi.vel.X) + Math.PI/2
+        break
+      case 'an':
+        newAngle = -Math.atan2(this.vel.Y - soi.vel.Y, this.vel.X - soi.vel.X) + 3*Math.PI/2
+        break
+      case 'ri':
+        newAngle = -Math.atan2(this.pos.Y - soi.pos.Y, this.pos.X - soi.pos.X) + Math.PI
+        break
+      case 'ro':
+        newAngle = -Math.atan2(this.pos.Y - soi.pos.Y, this.pos.X - soi.pos.X)
+        break
+      default:
+        break
     }
+
+    if (this.sas == 'na') {
+
+      this.angle += this.angularVelocity
+      if (this.angle > Math.PI * 2) {
+        this.angle -= Math.PI * 2
+      }
+    } else {
+      this.angle = newAngle
+      this.angularVelocity = 0
+    }
+
+
     // Limits the thrust between 0 and 1
+
     if (this.thrust > 1) {
       this.thrust = 1
     } else if (this.thrust < 0) {
       this.thrust = 0
     }
+
     this.acc.X += this.acceleration * this.thrust * Math.cos(this.angle)
     this.acc.Y -= this.acceleration * this.thrust * Math.sin(this.angle)
   }
@@ -264,9 +394,12 @@ class Player extends Object {
 function applyPhysics() {
   for (let i = 0; i < objects.length; i++){
     for (let j = i + 1; j < objects.length; j++){
-      objects[i].calculateGravity(objects[j]);
-      objects[i].calculateCollision(objects[j]);
-
+      if (doGravity) {
+        objects[i].calculateGravity(objects[j]);
+      }
+      if (doCollisions) {
+        objects[i].calculateCollision(objects[j]);
+      }
     }
   }
 }
@@ -281,14 +414,15 @@ function updateObjects() {
     objects[i].pos.addVector(objects[i].vel);
   }
 
-  calculateEnergy()
-
   for(let i = 0; i < objects.length; i++) {
     objects[i].acc.setZero()
   }
 
-  player.applyRocketControls();
   applyPhysics();
+  if (usePlayer) {
+    player.applyRocketControls();
+  }
+
 
   for(let i = 0; i < objects.length; i++) {
     objects[i].vel.X += objects[i].acc.X/2;
@@ -296,32 +430,21 @@ function updateObjects() {
   }
 }
 
-
-
-
-
-
-
 function renderObjects() {
   for(let i = 0; i < objects.length; i++) {
     objects[i].renderObject()
+
   }
 }
 
-function calculateEnergy(){
-  let total=0;
+function renderSymbols(){
   for (let i = 0; i < objects.length; i++){
-
-    kinetic = 0.5 * objects[i].mass * ((Math.hypot(objects[i].vel.Y, objects[i].vel.X))**2)
-    for (let j = i + 1; j < objects.length; j++){
-      let dist = Math.hypot(objects[i].pos.X - objects[j].pos.X, objects[i].pos.Y - objects[j].pos.Y);
-      grav = (GCONST * objects[i].mass * objects[j].mass) / dist
-      total -= grav
+    if (objects[i].radius * scale < minimumRadius) {
+      objects[i].renderSymbol()
     }
-    total += kinetic
   }
-  energy.innerText = total
 }
+
 
 function draw(time) {
   // Clears the canvas
@@ -330,18 +453,21 @@ function draw(time) {
   // Scales the camera view
   ctx.save()
   ctx.translate(centreX,centreY)
-  ctx.scale(1,1)
+  ctx.scale(scale,scale)
   ctx.translate(-centreX,-centreY)
 
   // Updates the positions and velocities of the objects
-  updateObjects();
+  // and repeats depending on the game speed
+  for (let i = 0; i < gameSpeed; i++){
+    updateObjects();
+  }
 
   // Renders the objects on the canvas
   renderObjects();
 
   ctx.restore()
 
-
+  renderSymbols();
 
   // Requests the next frame to be drawn,
   // creating a loop repeating 60 times per second
@@ -390,20 +516,60 @@ document.addEventListener("keyup", function (e) {
   }
 })
 
+
+function loadConfig() {
+  const JSONconfig = configInput.value
+  const config = JSON.parse(JSONconfig)
+
+  objects = []
+  if (usePlayer) {
+    player = new Player("red", 1, 1, new Vector(6.97 * 10 ** 8, 0), new Vector(0, 10 ** 4), new Vector(0, 0))
+    objects.push(player)
+  }
+
+  usePlayer = config[0][0]
+  doGravity = config[0][1]
+  doCollisions = config[0][2]
+
+  for (let i = 1; i < config.length; i++){
+    let card = config[i]
+    objects.push(new Object(
+      card[12],
+      card[2] * 10 ** card[3],
+      card[0] * 10 ** card[1],
+      new Vector(card[4] * 10 ** card[5], card[6] * 10 ** card[7]),
+      new Vector(card[8] * 10 ** card[9], card[10] * 10 ** card[11]),
+      new Vector(0, 0)
+    ))
+  }
+}
+
+function zoom(event) {
+  scale = Math.min(1,scale + (scaleFactor * event.deltaY * scale))
+}
+
+function increaseTime() {
+  gameSpeed *= 2
+  gameSpeedValue.innerText = gameSpeed
+
+}
+
+function decreaseTime() {
+  if (gameSpeed == 1) { return }
+  gameSpeed *= 0.5
+  gameSpeedValue.innerText = gameSpeed
+}
+
+function checkControls() {
+  player.sas = document.querySelector('input[name="rocketControls"]:checked').value;
+}
+
+body.onwheel = zoom
+
 function init() {
   draw();
 }
 
-let player = new Player("red", 1, 100, new Vector(1000, 0), new Vector(0, 0), new Vector(0,20))
-
-objects.push(new Object("blue", 100, 100, new Vector(-50000 + Math.random() * 100000, 50000 - Math.random() * 100000), new Vector(0, 0), new Vector(-10+(Math.random()*20), -10+(Math.random()*20))))
-objects.push(new Object("blue", 100, 10, new Vector(-50000 + Math.random() * 100000, 50000 - Math.random() * 100000), new Vector(0, 0), new Vector(-10+(Math.random()*20), -10+(Math.random()*20))))
 
 
-for (let i = 0; i < 50; i++) {
-  objects.push(new Object("blue", 0.01, 2, new Vector(-50 + Math.random() * 100, 50 - Math.random() * 100), new Vector(0, 0), new Vector((-0.5+Math.random())*4, (-0.5+Math.random())*4)))
-}
-
-
-objects.push(player)
 draw();
